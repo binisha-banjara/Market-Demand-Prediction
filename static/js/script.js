cb2139561a3be0ce08e1446a9fccfcc7ae0e89f1
binisha-banjara <rubiks1200@gmail.com>
2024-09-29 12:10:35 +0545

    form and model integrated for recommendation

--- static/js/script.js
+++ static/js/script.js
@@ -13,6 +13,31 @@ $(document).ready(function() {
   });
 });
 
+// Show Crop Price Prediction Form
+function showPredictionForm() {
+    document.getElementById('prediction-popup').style.display = 'flex';
+}
+
+// Show Crop Recommendation Form
+function showRecommendationForm() {
+    document.getElementById('recommendation-popup').style.display = 'flex';
+}
+
+// Close Crop Price Prediction Form
+function closePredictionForm() {
+    document.getElementById('prediction-popup').style.display = 'none';
+}
+
+// Close Crop Recommendation Form
+function closeRecommendationForm() {
+    document.getElementById('recommendation-popup').style.display = 'none';
+}
+
+function closePopup() {
+        $('#prediction-popup').hide();
+        $('#crop-form')[0].reset(); // Resets the form
+        $('#result').text(''); // Clears the result text
+    }
 
   
 window.onload = onPageLoad;
